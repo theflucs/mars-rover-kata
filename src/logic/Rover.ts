e0ef98cb59ad0b4d1973f9f50589afec0062a08e
Sabrina <89919203+theflucs@users.noreply.github.com>
2024-07-10 09:34:15 +0200

    Implement rover logic (#1)
    * create Rover class
    
    * feat: turn left and right with tests
    
    * feat: move forward and backward with tests
    
    * feat: pacman effect with tests
    
    * feat: handle obstacles with tests
    
    * refactor

---
+++ src/logic/Rover.ts
@@ -0,0 +1,73 @@
+import { Direction, Position } from "../types";
+import { MOVEMENTS, DIRECTIONS } from "../constants";
+import { getNextPosition, isObstacle } from "../utils";
+
+export class Rover {
+    private position: Position;
+    private direction: Direction;
+    private gridSize: [number, number];
+    private obstacles: Position[];
+
+    /**
+     * Create a Rover with default params.
+     * @param {Position} position
+     * @param {Direction} direction
+     * @param {[number, number]} gridSize
+     * @param {Position[]} [obstacles=[]]
+     */
+    constructor(position: Position, direction: Direction, gridSize: [number, number] = [5, 4], obstacles: Position[] = []) {
+        this.position = position;
+        this.direction = direction;
+        this.gridSize = gridSize;
+        this.obstacles = obstacles;
+    }
+
+    public getPosition(): Position {
+        return this.position;
+    }
+
+    public getDirection(): Direction {
+        return this.direction;
+    }
+
+    public setPosition(position: Position): void {
+        this.position = position;
+    }
+
+    public setDirection(direction: Direction): void {
+        this.direction = direction;
+    }
+
+    private turn(clockwise: boolean): void {
+        const currentIndex = DIRECTIONS.indexOf(this.direction);
+        const length = DIRECTIONS.length;
+
+        const newIndex = (currentIndex + (clockwise ? 1 : -1) + length) % length;
+
+        this.direction = DIRECTIONS[newIndex];
+    }
+
+    turnLeft(): void {
+        this.turn(false);
+    }
+
+    turnRight(): void {
+        this.turn(true);
+    }
+
+    private move(forward: boolean): void {
+        const nextPosition = getNextPosition(this.position, this.direction, forward, this.gridSize);
+
+        if (!isObstacle(nextPosition, this.obstacles)) {
+            this.position = nextPosition;
+        }
+    }
+
+    moveForward(): void {
+        this.move(true);
+    }
+
+    moveBackward(): void {
+        this.move(false);
+    }
+}
---
+++ src/utils.ts
@@ -0,0 +1,24 @@
+import { MOVEMENTS } from "./constants";
+import { Direction, Position } from "./types";
+
+export function isObstacle(position: Position, obstacles: Position[]): boolean {
+    return obstacles.some(([x, y]) => x === position[0] && y === position[1]);
+}
+
+export function getNextPosition(
+    currentPosition: Position,
+    direction: Direction,
+    forward: boolean,
+    gridSize: [number, number]
+): Position {
+    const [dx, dy] = MOVEMENTS.get(direction) || [0, 0];
+    const [x, y] = currentPosition;
+
+    const nextX = x + (forward ? dx : -dx);
+    const nextY = y + (forward ? dy : -dy);
+
+    const wrappedX = (nextX + gridSize[0]) % gridSize[0];
+    const wrappedY = (nextY + gridSize[1]) % gridSize[1];
+
+    return [wrappedX, wrappedY];
+}
