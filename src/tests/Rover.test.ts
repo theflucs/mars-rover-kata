024849ffc26533bf0d9341257b5d6e5a5f793d07
sabrinacux <sa.cux.dev@gmail.com>
2024-07-10 10:35:43 +0200

    refactor: extract utility functions tests in separate file

--- src/tests/Rover.test.ts
+++ src/tests/Rover.test.ts
@@ -1,5 +1,4 @@
 import { Rover } from "../logic/Rover";
-import { getNextPosition, isObstacle } from "../utils";
 import { Direction, GridSize, Position } from "../types";
 
 describe('Rover class', () => {
@@ -105,33 +104,6 @@ describe('Rover class', () => {
             rover = new Rover(start, direction as Direction, defaultGridSize);
             rover[method as 'moveForward' | 'moveBackward']();
             expect(rover.getPosition()).toEqual(end);
-        });
-    });
-
-    describe('utility functions', () => {
-        describe('getNextPosition', () => {
-            const gridSize: GridSize = [5, 4];
-
-            test.each([
-                [[0, 0], 'N', true, [0, 1]],
-                [[0, 0], 'N', false, [0, 3]],
-                [[4, 3], 'E', true, [0, 3]],
-            ])('should calculate the next position correctly', (start, direction, forward, expected) => {
-                expect(getNextPosition(start as Position, direction as Direction, forward, gridSize)).toEqual(expected);
-            });
-        });
-
-        describe('isObstacle', () => {
-            const obstacles: Position[] = [[2, 0], [0, 3], [3, 2]];
-
-            test.each([
-                [[2, 0], true],
-                [[0, 3], true],
-                [[3, 2], true],
-                [[0, 2], false],
-            ])('should correctly identify obstacles', (position, expected) => {
-                expect(isObstacle(position as Position, obstacles)).toBe(expected);
-            });
         });
     });
 });
