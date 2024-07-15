50cea1ee021bdcf50a734e83973ea19a028394ea
sabrinacux <sa.cux.dev@gmail.com>
2024-07-15 12:51:30 +0200

    refactor: update index.ts to execute main with input file path arg and add dev script

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('app running');
+import { main } from './main';
+
+const inputFilePath = process.argv[2];
+
+if (!inputFilePath) {
+    console.error('Please provide the input file path as an argument. Ex: "pnpm start src/input.txt"');
+    process.exit(1);
+}
+
+main(inputFilePath);
